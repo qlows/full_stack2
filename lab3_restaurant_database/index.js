bd6051c13b6c6d14dd3e4738358fc24e1b81f0f6
qlows <79415956+qlows@users.noreply.github.com>
2023-02-05 17:39:24 -0500

    Updated index page

--- lab3_restaurant_database/index.js
+++ lab3_restaurant_database/index.js
@@ -1,3 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const packageName = require('packageName');
+const restaurantRouter = require('./routes/restaurantRoute');
+const SERVER_PORT = 3000
+
+mongoose.connect("mongodb+srv://qlows:ananinamizuck@cluster0.hm9ineu.mongodb.net/restaurant_backend?retryWrites=true&w=majority",
+    {
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+    }
+)
+
+const app = express()
+
+// To return JSON
+app.use(express.json())
+
+app.use(restaurantRouter)
+
+app.route("/").get((req, res) => {
+    res.send("<h1>Restaurant backend</h1>")
+})
+
+app.listen(SERVER_PORT, () => {
+    console.log(`Server running at http://localhost:${SERVER_PORT}`)
+})
