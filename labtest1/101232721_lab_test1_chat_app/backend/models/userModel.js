829d8d69e68cde0262de1bb72e66b96192d648df
qlows <79415956+qlows@users.noreply.github.com>
2023-02-06 21:24:29 -0500

    Added user model

---
+++ labtest1/101232721_lab_test1_chat_app/backend/models/userModel.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    createdOn: {
+        type: Date,
+        default: Date.now()
+    },
+})
+
+module.exports = mongoose.model("users", userSchema)
