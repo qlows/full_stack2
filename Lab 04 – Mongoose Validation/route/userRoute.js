d0ae9dd87a36e1aa46e0ce1739b87a751882f508
qlows <79415956+qlows@users.noreply.github.com>
2023-02-13 00:01:48 -0500

    Changes to creating a user

--- Lab 04 – Mongoose Validation/route/userRoute.js
+++ Lab 04 – Mongoose Validation/route/userRoute.js
@@ -3,13 +3,13 @@ const userModel = require('../model/userModel');
 const app = express();
 
 // http://localhost:3000/users
-app.get('/users', async (req, res) => {
+app.post('/users', async (req, res) => {
+    const user = new userModel(req.body)
     try {
-        const users = await userModel.find();
-        res.json(users);
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
-});
+        await user.save()
+        res.status(201).send(user)
+    } catch (e) {
+        res.status(500).send(e.message)}
+})
 
 module.exports = app;
