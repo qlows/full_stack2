6fc610f8f4411e6cc6682a991d79270559d7eea8
qlows <79415956+qlows@users.noreply.github.com>
2023-02-12 23:30:48 -0500

    Added model

---
+++ Lab 04 – Mongoose Validation/model/userModel.js
@@ -0,0 +1,93 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please enter name'],
+        maxLength: [78, 'Your name cannot exceed 78 characters']
+    },
+    username: {
+        type: String,
+        required: [true, 'Please enter username'],
+        minLength: [4, 'Your username must be at least 4 characters'],
+    },
+    email: {
+        type: String,
+        required: [true, 'Please enter email'],
+        validate: function (value) {
+            var emailRegex = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+            return emailRegex.test(value);
+        },
+        message: props => `${props.value} is not an email address!`
+    },
+    address: {
+        street: {
+            type: String,
+            required: [true, 'Please enter street'],
+        },
+        suite: {
+            type: String,
+            required: [true, 'Please enter suite'],
+        },
+        city: {
+            type: String,
+            required: [true, 'Please enter city'],
+            validate: function (value) {
+                var cityRegex = /^[a-zA-Z]+(?:[\s-][a-zA-Z]+)*$/;
+                return cityRegex.test(value);
+            },
+        },
+        zipcode: {
+            type: Number,
+            required: [true, 'Please enter zipcode'],
+            validate: function (value) {
+                var zipRegex = /^\d{5}(?:[-\s]\d{4})?$/;
+                return zipRegex.test(value);
+            },
+            geo: {
+                lat: {
+                    type: Number,
+                    required: [true, 'Please enter latitude'],
+                },
+                lng: {
+                    type: Number,
+                    required: [true, 'Please enter longitude'],
+                }
+            }
+        },
+    },
+    phone: {
+        type: Number,
+        required: [true, 'Please enter phone'],
+        validate: function (value) {
+            var phoneRegex = /^\d{1}-\d{3}-\d{3}-\d{4}$/;            ;
+            return phoneRegex.test(value);
+        },
+        message: props => `${props.value} is not a phone number!`
+    },
+    website: {
+        type: String,
+        required: [true, 'Please enter website'],
+        validate: function (value) {            
+            var websiteRegex = /^(http|https):\/\/[^ "]+$/;
+            return websiteRegex.test(value);
+        },
+        message: props => `${props.value} is not a website!`
+    },
+    company: {
+        name: {
+            type: String,
+            required: [true, 'Please enter company name'],
+        },
+        catchPhrase: {
+            type: String,
+            required: [true, 'Please enter catch phrase'],
+        },
+        bs: {
+            type: String,
+            required: [true, 'Please enter bs'],
+        }
+    } 
+    });
+
+module.exports = mongoose.model('User', userSchema);
