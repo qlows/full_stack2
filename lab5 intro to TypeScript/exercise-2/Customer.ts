1e8332deb1665e52c62f51f985f8a717d930e609
qlows <79415956+qlows@users.noreply.github.com>
2023-03-10 17:04:32 -0500

    Minor Changes

--- lab5 intro to TypeScript/exercise-2/Customer.ts
+++ lab5 intro to TypeScript/exercise-2/Customer.ts
@@ -1,14 +1,14 @@
-class Customer {
-    firstName: string;
-    lastName: string;
+class Customer1 {
+    firstName1: string;
+    lastName1: string;
 
     public greeter() {
-        console.log(`Hello, ${this.firstName} ${this.lastName}`);
+        console.log(`Hello, ${this.firstName1} ${this.lastName1}`);
     }
 }
 
-let customer = new Customer();
-customer.firstName = "Dan";
-customer.lastName = "Blark";
-customer.greeter();
+let customer1 = new Customer1();
+customer1.firstName1 = "Dan";
+customer1.lastName1 = "Blark";
+customer1.greeter();
 
