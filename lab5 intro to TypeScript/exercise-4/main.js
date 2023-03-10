d9f34ebf1c8f14f0da87940ba593bab4c7939af7
qlows <79415956+qlows@users.noreply.github.com>
2023-03-10 17:38:48 -0500

    exec 4 main

---
+++ lab5 intro to TypeScript/exercise-4/main.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+var Customer_1 = require("./Customer");
+var customer = new Customer_1.Customer("Trey", "Wahlberg", 26);
+customer.greeter();
+customer.getAge();
---
+++ lab5 intro to TypeScript/exercise-4/main.ts
@@ -0,0 +1,5 @@
+import { Customer } from "./Customer";
+
+let customer = new Customer("Trey", "Wahlberg", 26);
+customer.greeter();
+customer.getAge();
